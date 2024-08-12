8f02a4d7f22328990493f660f2157d8ee671f38c
nyaki <narek1144@gmail.com>
2024-08-12 10:44:43 +0400

    Task-1

---
+++ task1.ts
@@ -0,0 +1,7 @@
+const firstName: string = "Jonatan";
+const age: number = 30;
+const tired: boolean = true;
+
+function data(name: string, age: number, tired: boolean): { name: string; age: number; tired: boolean } {
+  return { name, age, tired };
+}
